420f602239ebf5a572d55483b47c694429767605
Aayush <aayushkansal9214@gmail.com>
2024-05-09 01:53:24 +0530

    chnages in registration and login page

--- API/login.php
+++ API/login.php
@@ -1,58 +1,7 @@
 <?php
-// session_start();
-// require ("connect.php");
-
-// $mobile = $_POST['mobile'];
-// $password = $_POST['password'];
-// $role = $_POST['role'];
-
-// if (!$connect) {
-//     echo '
-//                 <script>
-//                     alert("Could not Login");
-//                 </script>
-//             ';
-// }
-
-// $check = mysqli_query($connect, "SELECT * FROM userdata WHERE mobile = '$mobile' AND binary password = '$password' AND role = '$role'");
-
-// if (mysqli_num_rows($check) > 0) {
-//     $usersdata = mysqli_fetch_array($check);
-//     $groups = mysqli_query($connect, "SELECT * FROM userdata WHERE role=2 ");
-//     $groupdata = mysqli_fetch_all($groups, MYSQLI_ASSOC);
-
-//     $_SESSION['usersdata'] = $usersdata;
-//     $_SESSION['groupdata'] = $groupdata;
-
-//     if ($role == 1) {
-//         echo '
-//                 <script>
-//                     window.location.replace("../Routes/dashboard.php");
-//                 </script>
-//             ';
-//     } else if ($role == 2) {
-//         echo '
-//                 <script>
-//                     window.location.replace("../Routes/dashboard.php");
-//                 </script>
-                
-//             ';
-//     }
-
-
-// } else {
-//     echo '
-//         <script>
-//             alert("User not found or Wrong details!");
-//             window.location = "../Login.html";
-//         </script>        
-//         ';
-// }
-
-
 
 session_start();
-require("connect.php");
+require ("connect.php");
 
 $mobile = $_POST['mobile'];
 $password = $_POST['password'];
@@ -66,54 +15,39 @@ if (!$connect) {
             ';
 }
 
-// Retrieve the hashed password from the database based on the provided mobile number and role
-$check = mysqli_query($connect, "SELECT * FROM userdata WHERE mobile = '$mobile' AND  role = '$role'");
+$check = mysqli_query($connect, "SELECT * FROM userdata WHERE mobile = '$mobile' AND binary password = '$password' AND role = '$role'");
+
 if (mysqli_num_rows($check) > 0) {
-    $userData = mysqli_fetch_assoc($check);
-    $hashedPassword = $userData['hash_password']; 
-    $verify = password_verify($password, $hashedPassword);
-    echo "Entered Password: " . $password . "<br>";
-    echo "Hashed Password from Database: " . $hashedPassword . "<br>";
-    echo "Password Verify Result: " . var_export($verify, true) . "<br>";    
-    $hashpass = password_hash($password, PASSWORD_BCRYPT);
-    echo'hashpass: '.$hashpass ;
-    // Verify the entered password against the hashed password
-    if ($verify) {
-        // Password is correct
-        $groups = mysqli_query($connect, "SELECT * FROM userdata WHERE role=2 ");
-        $groupdata = mysqli_fetch_all($groups, MYSQLI_ASSOC);
+    $usersdata = mysqli_fetch_array($check);
+    $groups = mysqli_query($connect, "SELECT * FROM userdata WHERE role=2 ");
+    $groupdata = mysqli_fetch_all($groups, MYSQLI_ASSOC);
 
-        $_SESSION['usersdata'] = $userData;
-        $_SESSION['groupdata'] = $groupdata;
+    $_SESSION['usersdata'] = $usersdata;
+    $_SESSION['groupdata'] = $groupdata;
 
-        // Redirect based on role
-        if ($role == 1 || $role == 2) {
-            echo '
+    if ($role == 1) {
+        echo '
                 <script>
                     window.location.replace("../Routes/dashboard.php");
                 </script>
             ';
-            exit();
-        }
-    } else {
-        // Incorrect password
+    } else if ($role == 2) {
         echo '
-            <script>
-                alert("Incorrect password!");
-                // window.location = "../Login.html";
-            </script>        
-        ';
-        exit();
+                <script>
+                    window.location.replace("../Routes/dashboard.php");
+                </script>
+                
+            ';
     }
+
+
 } else {
-    // User not found
     echo '
         <script>
             alert("User not found or Wrong details!");
             window.location = "../Login.html";
         </script>        
-    ';
-    exit();
+        ';
 }
 
 
--- API/registration.php
+++ API/registration.php
@@ -1,54 +1,5 @@
 <?php
-// include ("connect.php");
-
-// $name = $_POST['name'];
-// $number = $_POST['number'];
-// $password = $_POST['password'];
-// $gender = $_POST['gender'];
-// $confirmpassword = $_POST['confirmpassword'];
-// $address = $_POST['address'];
-// $image = $_FILES['photo']['name'];
-// $tmp_name = $_FILES['photo']['tmp_name'];
-// $role = $_POST['role'];
-
-// if ($password == $confirmpassword) {
-//     move_uploaded_file($tmp_name, "../uploads/$imag e");
-//     $mobiles = mysqli_query($connect, "select mobile from userdata where mobile =('$number')") or die(mysqli_error($con));
-//     if ($mobiles->num_rows > 0) {
-//         echo '
-//         <script> alert("Mobile number already exits");
-//         window.location = "../Routes/registeration.html";
-//         </script>
-//         ';
-//         exit();
-//     }
-//     $insert = mysqli_query($connect, "INSERT INTO userdata (name,mobile,password,gender,address,photo,role,status,votes) VALUE ('$name','$number','$password','$gender','$address','$image','$role',0,0)");
-//     if ($insert) {
-//         echo '
-//             <script>
-//                 alert("Registration Successful");
-//                 window.location = "../Login.html";
-//             </script>                
-//         ';
-//     } else {
-//         echo '
-//             <script>
-//                 alert("Some error occured!");
-//                 window.location = "../Routes/registeration.html";
-//             </script>                
-//         ';
-//     }
-// } else {
-//     echo '
-//             <script>
-//                 alert("password and confirm passowrd does not match");
-//                 window.location = "../Routes/registeration.html";
-//             </script>                
-//         ';
-// }
-
-
-include("connect.php");
+include ("connect.php");
 
 $name = $_POST['name'];
 $number = $_POST['number'];
@@ -61,21 +12,17 @@ $tmp_name = $_FILES['photo']['tmp_name'];
 $role = $_POST['role'];
 
 if ($password == $confirmpassword) {
-    // Hash the password
-    $hashed_password = password_hash($password, PASSWORD_BCRYPT);
-
-    move_uploaded_file($tmp_name, "../uploads/$image");
-
-    $mobiles = mysqli_query($connect, "SELECT mobile FROM userdata WHERE mobile = ('$number')") or die(mysqli_error($connect));
+    move_uploaded_file($tmp_name, "../uploads/$imag e");
+    $mobiles = mysqli_query($connect, "select mobile from userdata where mobile =('$number')") or die(mysqli_error($con));
     if ($mobiles->num_rows > 0) {
         echo '
-        <script> alert("Mobile number already exists");
+        <script> alert("Mobile number already exits");
         window.location = "../Routes/registeration.html";
         </script>
         ';
         exit();
     }
-    $insert = mysqli_query($connect, "INSERT INTO userdata (name,mobile,hash_password,gender,address,photo,role,status,votes) VALUES ('$name','$number','$hashed_password','$gender','$address','$image','$role',0,0)");
+    $insert = mysqli_query($connect, "INSERT INTO userdata (name,mobile,password,gender,address,photo,role,status,votes) VALUE ('$name','$number','$password','$gender','$address','$image','$role',0,0)");
     if ($insert) {
         echo '
             <script>
@@ -86,7 +33,7 @@ if ($password == $confirmpassword) {
     } else {
         echo '
             <script>
-                alert("Some error occurred!");
+                alert("Some error occured!");
                 window.location = "../Routes/registeration.html";
             </script>                
         ';
@@ -94,11 +41,12 @@ if ($password == $confirmpassword) {
 } else {
     echo '
             <script>
-                alert("Password and confirm password do not match");
+                alert("password and confirm passowrd does not match");
                 window.location = "../Routes/registeration.html";
             </script>                
         ';
 }
+
 
 
 ?>
