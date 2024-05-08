8f56fd5459a6c21f70674b761f19cd8a74f1b6fc
Aayush <aayushkansal9214@gmail.com>
2024-05-08 21:57:09 +0530

    chnage in password iin dahsboard and login page    also change in css  of login page and home page

--- API/login.php
+++ API/login.php
@@ -1,6 +1,58 @@
 <?php
+// session_start();
+// require ("connect.php");
+
+// $mobile = $_POST['mobile'];
+// $password = $_POST['password'];
+// $role = $_POST['role'];
+
+// if (!$connect) {
+//     echo '
+//                 <script>
+//                     alert("Could not Login");
+//                 </script>
+//             ';
+// }
+
+// $check = mysqli_query($connect, "SELECT * FROM userdata WHERE mobile = '$mobile' AND binary password = '$password' AND role = '$role'");
+
+// if (mysqli_num_rows($check) > 0) {
+//     $usersdata = mysqli_fetch_array($check);
+//     $groups = mysqli_query($connect, "SELECT * FROM userdata WHERE role=2 ");
+//     $groupdata = mysqli_fetch_all($groups, MYSQLI_ASSOC);
+
+//     $_SESSION['usersdata'] = $usersdata;
+//     $_SESSION['groupdata'] = $groupdata;
+
+//     if ($role == 1) {
+//         echo '
+//                 <script>
+//                     window.location.replace("../Routes/dashboard.php");
+//                 </script>
+//             ';
+//     } else if ($role == 2) {
+//         echo '
+//                 <script>
+//                     window.location.replace("../Routes/dashboard.php");
+//                 </script>
+                
+//             ';
+//     }
+
+
+// } else {
+//     echo '
+//         <script>
+//             alert("User not found or Wrong details!");
+//             window.location = "../Login.html";
+//         </script>        
+//         ';
+// }
+
+
+
 session_start();
-require ("connect.php");
+require("connect.php");
 
 $mobile = $_POST['mobile'];
 $password = $_POST['password'];
@@ -14,38 +66,55 @@ if (!$connect) {
             ';
 }
 
-$check = mysqli_query($connect, "SELECT * FROM userdata WHERE mobile = '$mobile' AND binary password = '$password' AND role = '$role'");
-
+// Retrieve the hashed password from the database based on the provided mobile number and role
+$check = mysqli_query($connect, "SELECT * FROM userdata WHERE mobile = '$mobile' AND  role = '$role'");
 if (mysqli_num_rows($check) > 0) {
-    $usersdata = mysqli_fetch_array($check);
-    $groups = mysqli_query($connect, "SELECT * FROM userdata WHERE role=2 ");
-    $groupdata = mysqli_fetch_all($groups, MYSQLI_ASSOC);
+    $userData = mysqli_fetch_assoc($check);
+    $hashedPassword = $userData['hash_password']; 
+    $verify = password_verify($password, $hashedPassword);
+    echo "Entered Password: " . $password . "<br>";
+    echo "Hashed Password from Database: " . $hashedPassword . "<br>";
+    echo "Password Verify Result: " . var_export($verify, true) . "<br>";    
+    $hashpass = password_hash($password, PASSWORD_BCRYPT);
+    echo'hashpass: '.$hashpass ;
+    // Verify the entered password against the hashed password
+    if ($verify) {
+        // Password is correct
+        $groups = mysqli_query($connect, "SELECT * FROM userdata WHERE role=2 ");
+        $groupdata = mysqli_fetch_all($groups, MYSQLI_ASSOC);
 
-    $_SESSION['usersdata'] = $usersdata;
-    $_SESSION['groupdata'] = $groupdata;
+        $_SESSION['usersdata'] = $userData;
+        $_SESSION['groupdata'] = $groupdata;
 
-    if ($role == 1) {
-        echo '
+        // Redirect based on role
+        if ($role == 1 || $role == 2) {
+            echo '
                 <script>
                     window.location.replace("../Routes/dashboard.php");
                 </script>
             ';
-    } else if ($role == 2) {
+            exit();
+        }
+    } else {
+        // Incorrect password
         echo '
-                <script>
-                    window.location.replace("../Routes/dashboard.php");
-                </script>
-                
-            ';
+            <script>
+                alert("Incorrect password!");
+                // window.location = "../Login.html";
+            </script>        
+        ';
+        exit();
     }
-
-
 } else {
+    // User not found
     echo '
         <script>
             alert("User not found or Wrong details!");
             window.location = "../Login.html";
         </script>        
-        ';
+    ';
+    exit();
 }
+
+
 ?>
