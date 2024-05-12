4d42e839403f2b765f43831e8ea14587512b4214
Aayush <aayushkansal9214@gmail.com>
2024-05-12 23:46:36 +0530

    changes

---
+++ JS/admin.js
@@ -0,0 +1,6 @@
+function open_sidebar() {
+    document.getElementById("open_sidebar").style.marginLeft = "25%";
+    document.getElementById("mySidebar").style.width = "25%";
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("open_sidebar").style.display = 'none';
+}
