05e988e28990db5360ef38fd134cafbe0a36c765
David-Singh-Rolson <davidsinghrolson@gmail.com>
2024-07-05 11:45:31 +0530

    fixes

--- admin/admin_asset/pass.php
+++ admin/admin_asset/pass.php
@@ -1,4 +1,5 @@
 <?php
+$admin_user='admin';
 $password = 'admin@123';
 $hashed_password = password_hash($password, PASSWORD_BCRYPT);
 echo $hashed_password;
--- admin/connect.php
+++ admin/connect.php
@@ -4,23 +4,3 @@
 $db = new SQLite3('../db/online_voting_system.db');
 
 
-// date_default_timezone_set('Asia/Kolkata');
-
-// $currentDatetime = date('Y-m-dTH:i:s');
-
-// $query = "SELECT * FROM meta";
-// $result = $db->querySingle($query);
-
-// $election_is_live = false;
-
-// if ($result != false) {
-//     if ($currentDatetime < $result) {
-//         $election_is_live = true;
-//     }
-// } else {
-//     echo "Error retrieving datetime from database.";
-// }
-
-// // echo "<script>var is_election_live = '" . $election_is_live . "';</script>";
-
-// // $election_is_live = $result;
